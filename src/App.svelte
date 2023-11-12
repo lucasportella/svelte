b036f203c8b21912993496dc7799754a45d6b6b6
lucas <lucasportellaagu@gmail.com>
2023-11-12 12:38:43 -0300

    feat(svelte): initial commit with input bind

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	 let name = 'Lucas';
+	 const handleInput = (e) => {
+		name = e.target.value
+	 }
+	 const handleClick = () => {
+		name = 'new title'
+	 }
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={handleClick}>update title</button>
+	<input on:input={handleInput} bind:value={name}/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
